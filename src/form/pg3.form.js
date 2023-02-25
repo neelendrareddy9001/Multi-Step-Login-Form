0e7c96967b90ec01266aeedbd5e1fbe58ea9f7be
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-02-25 23:00:16 +0100

    Update pg3.form.js

--- src/form/pg3.form.js
+++ src/form/pg3.form.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
 export const Pg3 = () => {
-    return (
-        <div className='container'>
+    const {password, rPassword, setPassword, setRPassword} = useContext(FormContext);
+  <div className='container'>
       <form>
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Password</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value={password} onChange={(e) => setPassword(e.target.value)} />
   </div>
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label"> Repeat Password</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value={password} onChange={(e) => setRPassword(e.target.value)}/>
   </div>
   
 </form>
