7373e0ec1ad293acaf9ae680d57e17f93ffacdd0
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-02-25 22:56:18 +0100

    Update pg1.form.js

--- src/form/pg1.form.js
+++ src/form/pg1.form.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, {useContext} from 'react';
 
 export const Pg1 = () => {
+    const { } = useContext(FormContext);
     return (
         <div className='container'>
       <form>
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Username</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value={userName}
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }} />
   </div>
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label"> Repeat Username</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value={cUsername}
+          onChange={(e) => {
+            setCUsername(e.target.value);
+          }} />
   </div>
  
   
